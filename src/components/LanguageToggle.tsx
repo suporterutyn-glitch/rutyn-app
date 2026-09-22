10f6d21c6e5500491988c56b4e8e00654fc10f38
Jonathan Delgado <jonas@MacBook-Air-de-Jonathan.local>
2026-09-22 14:11:32 -0300

    Add debug log to language toggle

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -33,6 +33,7 @@ export function LanguageToggle() {
     const idx = LANGS.indexOf(currentLang)
     const nextIdx = (idx + 1) % LANGS.length
     const next = LANGS[nextIdx]
+    console.log({ stored, currentLang, idx, nextIdx, next, LANGS })
     await setLang(next)
   }
 
